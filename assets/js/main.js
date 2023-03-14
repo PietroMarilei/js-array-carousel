e6eb0cf3c78050b8ab9801081e4550094e50ad69
PietroMarilei~ <pietromrl0@gmail.com>
2023-03-14 15:41:44 +0100

    semi-working Js

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,9 +21,9 @@ let activeImg = 0;
 for (let i = 0; i < imgArray.length; i++) {
     let imgSrc = imgArray[i];
     // imgSrc va a prendere l'img in posizione i
-    const imgElement = `<img class="img-fluid rounded-2 ${i === activeImg ? 'active' : ''}" src="${imgSrc}" alt="">`
+    const imgElement = `<img class="img-fluid rounded-2 ${i === activeImg ? 'active' : ''}" src="${imgSrc}" alt="">`;
     // creo la const imgElement, che dentro ha un template literal
-    // se la posizione dell'array é 0 allora metto active come classe, altrimenti non metto niente
+    // alla posizione corrispondente al valore di activeImg (che dopo aumenta col bottone) allora metto active come classe, altrimenti non metto niente
     // non ho tanto capito a cosa serve sta cosa
 
     console.log(imgElement);
@@ -34,7 +34,7 @@ for (let i = 0; i < imgArray.length; i++) {
 
 }
 
-const slideImagesElement = document.querySelectorAll('.slider > .images > img');
+const sliderImagesElement = document.querySelectorAll('.slider > .images > img');
 // seleziono l'immagine in pagina
 // perché non sono tutte ?
 
@@ -45,13 +45,38 @@ const nextEl = document.querySelector('.next');
 nextEl.addEventListener("click", function () {
     console.log('cliccato next');
 
-    const currentSlide = slideImagesElement[activeImg];
+    const currentSlide = sliderImagesElement[activeImg];
     console.log("questa é la slide corrente " + currentSlide);
     // seleziono la slide in posizione activeImg = 0
+    currentSlide.classList.remove('active');
+    // tolgo la classe active alla img in pagina
+    activeImg++
+    // finalmente incremento il valore di activeImg cliccando sul bottone
+    console.log(activeImg);
+    // con questo log vedo che activeImg aumenta al click
+    const nextImage = sliderImagesElement[activeImg];
+    // seleziono la prossima imagine che é diversa da prima perché activeImg é aumentato di 1
+    nextImage.classList.add('active')
+    // gli do la classe active
 
+})
 
+const prevEl = document.querySelector(".prev")
+// seleziono il bottone prev
+prevEl.addEventListener('click', function () {
+    console.log('cliccato next');
 
-
-
-
-}) 
+    const currentSlide = sliderImagesElement[activeImg];
+    console.log("questa é la slide corrente " + currentSlide);
+    // seleziono la slide in posizione activeImg = 0
+    currentSlide.classList.remove('active');
+    // tolgo la classe active alla img in pagina
+    activeImg--
+    // decremento il valore di activeImg
+    console.log(activeImg);
+    // con questo log vedo che activeImg decresce al click
+    const nextImage = sliderImagesElement[activeImg];
+    // seleziono la prossima imagine che é diversa da prima perché activeImg é aumentato di 1
+    nextImage.classList.add('active');
+    // gli do la classe active
+})
